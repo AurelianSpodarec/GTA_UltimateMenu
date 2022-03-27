0f9b4c16fa6d718c998db2584608d2b7345adc69
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-03-27 07:38:54 +0100

    Added readme and a few updates

--- Menu/Army/index.lua
+++ Menu/Army/index.lua
@@ -1,234 +1,45 @@
+local Spawn = require('GTA_LawEnforcement.Utils.Spawn')
+local MenuData = require('GTA_LawEnforcement.Data.MenuData')
+
 require("GTA_LawEnforcement/Menu/Army/features/armyGodMode")
 require("GTA_LawEnforcement/Menu/Army/features/armyJets")
 require("GTA_LawEnforcement/Menu/Army/features/armyPersonel")
 require("GTA_LawEnforcement/Menu/Army/features/armyTanks")
 require("GTA_LawEnforcement/Menu/Army/features/armyVehicles")
-local Spawn = require('GTA_LawEnforcement.Utils.Spawn')
-
-
-
-
-
-
- 
--- local armmySubMenu = {
---     {
---         'name' = "Spawn Military Vehicles",
---         'parent' = false,
---         'function' = nil
---     }
--- }
--- loop over it and create a menu like that instead
-
---  local militaryVehicles = {
---     {
---         model = "RHINO", 
---         hash = 782665360, 
---         name = "Rhino", 
---         category = "Car"
---     },
---     {
---         model = "BARRACKS", 
---         hash = 3471458123, 
---         name = "Barracks", 
---         category = "Car"
---     },
--- }
-
-
--- get model by name 
-
--- function createMenuItem(item, featureType, parentID, functionality)
---     add_feature(item, featureType, parentID, functionality)
--- end
-
-
--- function spawnVehicle(hash)
---     Spawn.Vehicle(hash, player.get_player_coords(player.player_id()), entity.get_entity_heading(player.get_player_ped(player.player_id()))
--- end
-
-
-
--- Army 
--- Vehicles
--- - Combat
--- -- tank, tank2 etc
--- - Planes
--- -- lazer, hydra, byro
--- - Cars
--- etc
-
-    
-
-local menuData = {
-    military = {
-        name = "Military",
-        spawn_vehicles = {
-            name = "Spawn Vehicles",
-            children = {
-                {
-                    name = "Cars",
-                    children = {
-                        {
-                            name = 'Ramp Buggy',
-                            hash = 0xCEB28249
-                        },
-                    },
-                },
-                {
-                    name = "Planes",
-                    children = {
-                        {
-                            name = 'Akula',
-                            hash = 0x46699F47
-                        },
-                        {
-                            name = 'Annihilator',
-                            hash = 0x31F0B376
-                        },
-                        {
-                            name = 'Buzzard',
-                            hash = 0x2F03547B
-                        },
-                        {
-                            name = 'Hunter',
-                            hash = 0xFD707EDE
-                        },
-                        {
-                            name = 'Savage',
-                            hash = 0xFB133A17
-                        },
-                        {
-                            name = 'Valkyrie',
-                            hash = 0xA09E15FD
-                        },
-                        {
-                            name = 'Valkyrie 2',
-                            hash = 0x5BFA5C4B
-                        },
-                        {
-                            name = 'Annihilator 2',
-                            hash = 0x11962E49
-                        },
-                        {
-                            name = 'Avenger',
-                            hash = 0x81BD2ED0
-                        },
-                        {
-                            name = 'Bombushka',
-                            hash = 0xFE0A508C
-                        },
-                        {
-                            name = 'Hydra',
-                            hash = 0x39D6E83F
-                        },
-                        {
-                            name = 'Lazer',
-                            hash = 0xB39B0AE6
-                        },
-                        {
-                            name = 'B-11 Strikeforce',
-                            hash = 0x64DE07A1
-                        },
-                        {
-                            name = 'Alkonost',
-                            hash = 0xEA313705
-                        }
-                    }
-                },
-                {
-                    name = "Combat Vehicle",
-                    children = {
-                        {
-                            model = "RHINO", 
-                            hash = 782665360, 
-                            name = "Rhino", 
-                            category = "Car"
-                        },
-                        {
-                            model = "BARRACKS", 
-                            hash = 3471458123, 
-                            name = "Barracks", 
-                            category = "Car"
-                        },
-                        {
-                            name = 'APC',
-                            hash = 0x2189D250
-                        },
-                    }
-                }
-            }
-        },
-        trolling = {
-            name = "Trolling",
-            children = {
-                -- attack
-            }
-        },
-        peaceful = {
-            name = "Peaceful",
-            children = {
-                -- protection
-            }
-        }
-    },
-    police = {
-        name = "Police",
-        children = {
-
-        }
-    }
-
-}
-
--- function spawnByHash(hash)
---     Spawn.Vehicle(hash, player.get_player_coords(player.player_id()), entity.get_entity_heading(player.get_player_ped(player.player_id()))
--- end
 
 -- backup
 -- swat, police, army: will follow around X player as bodyguards, if someone attacks, they go chase after them
 
-
 function createArmyMenu(parent, config)
 
     
-    militarySubmenu = menu.add_feature(menuData.military.name, "parent", parent.id, nil)
+    militarySubmenu = menu.add_feature(MenuData.military.name, "parent", parent.id, nil)
 
 
-    militarySpawnVehice = menu.add_feature(menuData.military.spawn_vehicles.name, "parent", militarySubmenu.id, nil)
-    for index = 1, #menuData.military.spawn_vehicles.children do
+    militarySpawnVehice = menu.add_feature(MenuData.military.spawn_vehicles.name, "parent", militarySubmenu.id, nil)
+    for index = 1, #MenuData.military.spawn_vehicles.children do
 
-        local item = menuData.military.spawn_vehicles.children[index]
+        local item = MenuData.military.spawn_vehicles.children[index]
         local parent = menu.add_feature(item.name, "parent", militarySpawnVehice.id, nil)
 
         for j = 1, #item.children do
             local submenu = item.children[j]
-
             menu.add_feature(submenu.name, "action", parent.id,  function(f) 
-                -- Spawn.Vehicle(782665360, player.get_player_coords(player.player_id()), entity.get_entity_heading(player.get_player_ped(player.player_id()))
-                Spawn.Vehicle(submenu.hash, player.get_player_coords(player.player_id()), entity.get_entity_heading(player.get_player_ped(player.player_id())))
+                Spawn.Vehicle(submenu.hash, player.get_player_coords(player.player_id()))
             end)
-            -- spawn the vehicle
         end
 
     end
 
 
-    militaryTrolling = menu.add_feature(menuData.military.trolling.name, "parent", militarySubmenu.id, nil)
-    militaryPeaceful = menu.add_feature(menuData.military.peaceful.name, "parent", militarySubmenu.id, nil)
+    militaryTrolling = menu.add_feature(MenuData.military.trolling.name, "parent", militarySubmenu.id, nil)
+    militaryPeaceful = menu.add_feature(MenuData.military.peaceful.name, "parent", militarySubmenu.id, nil)
 
 
 end
 
      
-    
-
-    -- for key in pairs(militaryVehicles) do
-    --     menu.add_feature(militaryVehicles[key]['name'], "action", armySubmenu_vehicles.id,  Spawn.Vehicle(militaryVehicles[key]['hash'], player.get_player_coords(player.player_id()), entity.get_entity_heading(player.get_player_ped(player.player_id())))
-    -- )
-    -- end
-    
-   
+     
 
 
     -- armySubmenu_godMode = menu.add_feature('God Mode', "toggle", armySubmenu.id, armyGodMode)
---
+++ README.md
@@ -0,0 +1,115 @@
+# GTA Law Enforcement 
+
+
+## Taxi
+- Taxi arrives taxi and goes from point A to point B
+
+## Friendly
+Give Godmode
+
+
+
+## Motorcades
+- Simpe Highway Patrol:
+1 police cruiser
+2 motorcycle
+player
+1 swat team
+two motorcylce at the bottom
+
+
+## Chat Commands/Send SMS
+#Vechicle
+(Vehicle) Stop
+- This is SWAT! Park your vehicle now!
+- Backup on your way!
+- Stop flying or you will be shot!
+- Smile to the camera! 
+
+#Vehicle Stopped
+- License and registration
+- Plese step out from your vehicle
+- Thank you for coperatoin
+
+
+## Military
+Spawn Military Vehicles
+God Mode
+Backup (1-5)  (2tanks, 3jets, 2 helicopters, 1volator, 3military cars)
+Grief/Protect
+- God Mode
+- Tank (1-5)
+- Jets (1-5)
+- Helicopter (1-5)
+- Military Cars
+- Volator (1-5)
+- Personel (1-30) - with cars
+
+
+## Police
+Spawn Police Vehicles
+God Mode
+Backup (1-5) (4swat units, 6hightway patrol, 2 police cruise 2 helicopters) - 1-5 multiplies changes the values of all
+Grief/Protect
+- God Mode
+- SWAT
+- Highway patrol - motorcycle (1-5)
+- Police Cruise (1-5)
+- Helicopter
+
+
+
+
+## Enforcement
+
+Create Units -  e.g FBI car and 8 SWAT team, a higway patrol, etc...
+
+Backup - spawns units that will die
+Protection spawns bodyguards for player
+Attack - attacks a certain player, or attacks everyone if they come to range 
+
+
+
+<!-- 
+Ability to enable/disable
+
+SMS phases 
+
+#Griefing 
+(attack sent)
+- Surrender now! The National Security forces have been deployed!
+
+
+#Help
+(on setting protection)
+- To protect and serve
+
+(units death)
+- Mission failed comare, we'll get it next time! 
+- The fight might be lost, but the battle is not over yet!
+
+(units dying)
+- We are losing units! Takve cover!
+- Takve cover now!
+
+(upon killing a player - 20% chance in happening)
+- Were spraying them!
+- Good job! 
+
+-->
+
+
+ 
+  
+
+
+
+
+License
+Copyright (c) 2022 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
