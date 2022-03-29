cdb1a03649e96b5150d3261ea8559a0c20ebc5b7
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-03-29 19:07:39 +0100

    Updates

--- Data/weapons.lua
+++ Data/weapons.lua
@@ -1,396 +1,225 @@
 -- https://wiki.gtanet.work/index.php?title=Weapons_Models
 
 -- /Notes
--- Doing the data this way is a bit limting
--- Could reate a list of all weapons and give it a 'category' id
--- To give the items order  in category, can give them a 'category_item_order' id and organize it like that
--- Then write a function to get all wepons from X category
--- That way the database is more dynamic - maybe
+-- Doing the data this way is a bit lmting {
+        -- Could reate a list of all weapons and give it a 'category' id    {
+
+            -- To give the items order  in catgory, can give them a 'category_item_order' id and organize it like that
+            -- Then write a function to ge,t all wepons from X category
+                -- That way the database is more dynamic - maybe
+ 
+ 
+
 
 local weapons = {
 
     {
         name = "Melee",
         children = {
-
-            {
-                name = "Unarmed",
-                hash = -1569615261
-            },
-            {
-                name = "Knife",
-                hash = -1716189206
-            },
-            {
-                name = "Nightstick",
-                hash = 1737195953
-            },
-            {
-                name = "Hammer",
-                hash = 1317494643
-            },
-            {
-                name = "Bat",
-                hash = -1786099057
-            },
-            {
-                name = "Crowbar",
-                hash = -2067956739
-            },
-            {
-                name = "Golfclub",
-                hash = 1141786504
-            },
-            {
-                name = "Bottle",
-                hash = -102323637
-            },
-            {
-                name = "Dagger",
-                hash = -1834847097
-            },
-            {
-                name = "Hatchet",
-                hash = -102973651
-            },
-            {
-                name = "KnuckleDuster",
-                hash = -656458692
-            },
-            {
-                name = "Machete",
-                hash = -581044007
-            },
-            {
-                name = "Flashlight",
-                hash = -1951375401
-            },
-            {
-                name = "SwitchBlade",
-                hash = -538741184
-            },
-            {
-                name = "Poolcue",
-                hash = -1810795771
-            },
-            {
-                name = "Wrench",
-                hash = 419712736
-            },
-            {
-                name = "Battleaxe",
-                hash = -853065399
-            } 
-
-        }
-    },
-
-    {
-        name = "Handguns",
-        children = {
-
-            {        
-                name = "Pistol",
-                hash = 453432689
-            },
-            {
-                name = "Pistol Mk2",
-                hash = 3219281620
-            },
-            {
-                name = "Combat Pistol",
-                hash = 1593441988
-            },
-            {
-                name = "Pistol 50",
-                hash = -1716589765
-            },
-            {
-                name = "SNS Pistol",
-                hash = -1076751822
-            },
-            {
-                name = "Heavy Pistol",
-                hash = -771403250
-            },
-            {
-                name = "Vintage Pistol",
-                hash = 137902532
-            },
-            {
-                name = "Marksman Pistol",
-                hash = -598887786
-            },
-            {
-                name = "Revolver",
-                hash = -1045183535
-            },
-            {
-                name = "AP Pistol",
-                hash = 584646201
-            },
-            {
-                name = "Stun Gun",
-                hash = 911657153
-            },
-            {
-                name = "Flare Gun",
-                hash = 1198879012
-            }
-
-        }
-    },
-
-    {
-        name = "Machine Guns",
-        children = {
-          
-            {            
-                name = "MicroSMG",
-                hash = 324215364
-            },
-            {
-                name = "Machine Pistol",
-                hash = -619010992
-            },
-            {
-                name = "SMG",
-                hash = 736523883
-            },
-            {
-                name = "SMG - Mk2",
-                hash = 2024373456
-            },
-            {
-                name = "Assault SMG",
-                hash = -270015777
-            },
-            {
-                name = "Combat PDW",
-                hash = 171789620
-            },
-            {
-                name = "MG",
-                hash = -1660422300
-            },
-            {
-                name = "Combat MG",
-                hash = 2144741730
-            },
-            {
-                name = "Combat MG - Mk2",
-                hash = 3686625920
-            },
-            {
-                name = "Gusenberg",
-                hash = 1627465347
-            },
-            { 
-                name = "MiniSMG",
-                hash = -1121678507
-            }
-
-        }
-    },
-
-    {
-        name = "Assault Rifles",
-        children = {
-
-            {
-                name = "AssaultRifle",
-                hash = -1074790547
-            },
-            {
-                name = "AssaultRifleMk2",
-                hash = 961495388
-            },
-            {
-                name = "CarbineRifle",
-                hash = -2084633992
-            },
-            { 
-                name = "CarbineRifleMk2",
-                hash = 4208062921
-            },
-            {
-                name = "AdvancedRifle",
-                hash = -1357824103
-            },
-            {
-                name = "SpecialCarbine",
-                hash = -1063057011
-            },
-            {
-                name = "BullpupRifle",
-                hash = 2132975508
-            },
-            {
-                name = "CompactRifle",
-                hash = 1649403952
-            }
-
-        }
-    },
-
-    {
-        name = "Sniper Rifles",
-        children = {
-
-            {               
-                name = "SniperRifle",
-                hash = 100416529
-            },
-            {
-                name = "HeavySniper",
-                hash = 205991906
-            },
-            {
-                name = "HeavySniperMk2",
-                hash = 177293209
-            },
-            {
-                name = "MarksmanRifle",
-                hash = -952879014
-            }
-
-        }
-    },
-
-    {
-        name = "Shotguns",
-        children = {
-            
-            {            
-                name = "PumpShotgun",
-                hash = 487013001
-            },
-            {
-                name = "SawnoffShotgun",
-                hash = 2017895192
-            },
-            {
-                name = "BullpupShotgun",
-                hash = -1654528753
-            },
-            {
-                name = "AssaultShotgun",
-                hash = -494615257
-            },
-            {
-                name = "Musket",
-                hash = -1466123874
-            },
-            {
-                name = "HeavyShotgun",
-                hash = 984333226
-            },
-            {
-                name = "DoubleBarrelShotgun",
-                hash = -275439685
-            },
-            {
-                name = "Autoshotgun",
-                hash = 317205821
-            }
-
-        }
-    },
-
-    {
-        name = "Heavy Weapons",
-        children = {
                 
             {
-                name = "GrenadeLauncher",
-                hash = -1568386805
+                ID = "weapon_dagger",
+                name = "Antique Cavalry Dagger",
+                hash = 0x92A27487
+            },   
+            {
+                ID = "weapon_bat",
+                name = "Baseball Ball",
+                hash = 0x958A4A8F
             },
             {
-                name = "RPG",
-                hash = -1312131151
+                ID = "weapon_bottle",
+                name = "Broken Bottle",
+                hash = 0xF9E6AA4B
             },
             {
-                name = "Minigun",
-                hash = 1119849093
+                ID = "weapon_crowbar",
+                name = "Crowbar",
+                hash = 0x84BD7BFD
             },
             {
-                name = "Firework",
-                hash = 2138347493
+                ID = "weapon_unarmed",
+                name = "Fist",
+                hash = 0xA2719263
             },
             {
-                name = "Railgun",
-                hash = 1834241177
+                ID = "weapon_flashlight",
+                name = "Flashlight",
+                hash = 0x8BB05FD7
             },
             {
-                name = "HomingLauncher",
-                hash = 1672152130
+                ID = "weapon_golfclub",
+                name = "Golf Club",
+                hash = 0x440E4788
             },
             {
-                name = "GrenadeLauncherSmoke",
-                hash = 1305664598
+                ID = "weapon_hammer",
+                name = "Hammer",
+                hash = 0x4E875F73
             },
             {
-                name = "CompactLauncher",
-                hash = 125959754
+                ID = "weapon_hatchet",
+                name = "Hatchet",
+                hash = 0xF9DCBF2D
+            },
+            {
+                ID = "weapon_knuckle",
+                name = "Brass Knuckles",
+                hash = 0xD8DF3C3C
+            },
+            {
+                ID = "weapon_knife",
+                name = "Knife",
+                hash = 0x99B507EA
+            },
+            {
+                ID = "weapon_machete",
+                name = "Machete",
+                hash = 0xDD5DF8D9
+            },
+            {
+                ID = "weapon_switchblade",
+                name = "Switchblade",
+                hash = 0xDFE37640
+            },
+            {
+                ID = "weapon_nightstick",
+                name = "Nightstick",
+                hash = 0x678B81B1
+            },
+            {
+                ID = "weapon_wrench",
+                name = "Pipe Wrench",
+                hash = 0x19044EE0
+            },
+            {
+                ID = "weapon_battleaxe",
+                name = "Battle Axe",
+                hash = 0xCD274149
+            },
+            {
+                ID = "weapon_poolcue",
+                name = "Pool Cue",
+                hash = 0x94117305
+            },
+            {
+                ID = "weapon_stone_hatchet",
+                name = "Stone Hatchet",
+                hash = 0x3813FC0   
             }
 
+        },
+
+        {
+            name = "Handguns",
+            children = {
+
+                {                
+                    ID = "weapon_pistol",
+                    name = "Pistol",
+                    hash = 0x1B06D571
+                }
+                -- { 
+                --     ID = "weapon_pistol_mk2",
+                --     name = "Pistol Mk II",
+                --     hash = 0xBFE256D4
+                -- },
+                -- { 
+                --     ID = "weapon_combatpistol",
+                --     name = "Combat Pistol",
+                --     hash = 0x5EF9FEC4
+                -- },
+                -- { 
+                --     ID = "weapon_appistol",
+                --     name = "AP Pistol",
+                --     hash = 0x22D8FE39
+                -- },
+                -- { 
+                --     ID = "weapon_stungun",
+                --     name = "Stun Gun",
+                --     hash = 0x3656C8C1
+                -- },
+                -- { 
+                --     ID = "weapon_pistol50",
+                --     name = "Pistol .50",
+                --     hash = 0x99AEEB3B
+                -- },
+                -- { 
+                --     ID = "weapon_snspistol",
+                --     name = "SNS Pistol",
+                --     hash = 0xBFD21232
+                -- },
+                -- { 
+                --     ID = "weapon_snspistol_mk2",
+                --     name = "SNS Pistol Mk II",
+                --     hash = 0x88374054
+                -- },
+                -- { 
+                --     ID = "weapon_heavypistol",
+                --     name = "Heavy Pistol",
+                --     hash = 0xD205520E
+                -- },
+                -- { 
+                --     ID = "weapon_vintagepistol",
+                --     name = "Vintage Pistol",
+                --     hash = 0x83839C4
+                -- },
+                -- { 
+                --     ID = "weapon_flaregun",
+                --     name = "Flare Gun",
+                --     hash = 0x47757124
+                -- },
+                -- { 
+                --     ID = "weapon_marksmanpistol",
+                --     name = "Marksman Pistol",
+                --     hash = 0xDC4DB296
+                -- },
+                -- { 
+                --     ID = "weapon_revolver",
+                --     name = "Heavy Revolver",
+                --     hash = 0xC1B3C3D1
+                -- },
+                -- { 
+                --     ID = "weapon_revolver_mk2",
+                --     name = "Heavy Revolver Mk II",
+                --     hash = 0xCB96392F
+                -- },
+                -- { 
+                --     ID = "weapon_doubleaction",
+                --     name = "Double Action Revolver",
+                --     hash = 0x97EA20B8
+                -- },
+                -- { 
+                --     ID = "weapon_raypistol",
+                --     name = "Up-n-Atomizer",
+                --     hash = 0xAF3696A1
+                -- },
+                -- { 
+                --     ID = "weapon_ceramicpistol",
+                --     name = "Ceramic Pistol",
+                --     hash = 0x2B5EF5EC
+                -- },
+                -- { 
+                --     ID = "weapon_navyrevolver",
+                --     name = "Navy Revolver",
+                --     hash = 0x917F6C8C
+                -- },
+                -- { 
+                --     ID = "weapon_gadgetpistol",
+                --     name = "Perico Pistol",
+                --     hash = 0x57A4368C
+                -- },
+                -- {
+                --     ID = "weapon_stungun_mp",
+                --     name = "Stun Gun",
+                --     hash = 0x45CD9CF3
+                -- }
+
+             }
+
         }
-    },
 
-    {
-        name = "Thrown Weapons",
-        children = {
 
-            {     
-                name = "Grenade",
-                hash = -1813897027
-            },
-            {
-                name = "StickyBomb",
-                hash = 741814745
-            },
-            {
-                name = "ProximityMine",
-                hash = -1420407917
-            },
-            {
-                name = "BZGas",
-                hash = -1600701090
-            },
-            {
-                name = "Molotov",
-                hash = 615608432
-            },
-            {
-                name = "FireExtinguisher",
-                hash = 101631238
-            },
-            {
-                name = "PetrolCan",
-                hash = 883325847
-            },
-            {
-                name = "Flare",
-                hash = 1233104067
-            },
-            {
-                name = "Ball",
-                hash = 600439132
-            },
-            {
-                name = "Snowball",
-                hash = 126349499
-            },
-            {
-                name = "SmokeGrenade",
-                hash = -37975472
-            },
-            {
-                name = "Pipebomb",
-                hash = -1169823560
-            }
-
-        }
     }
 
 }
--- models/ModelPed.lua
+++ models/ModelPed.lua
@@ -97,7 +97,7 @@ function ModelPed.setAllWeapon()
     for i = 1, #peds do
         if not ped.is_ped_a_player(peds[i]) then
             if HP_Entity.request_control(peds[i], 25) then
-                weapon.give_delayed_weapon_to_ped(peds[i], 0x476BF155, 10000, true)
+                weapon.give_delayed_weapon_to_ped(peds[i], 0xBFE256D4, 10000, true)
             end
         end
     end 
@@ -117,18 +117,6 @@ function ModelPed.setWeapon(ult_weapon)
     end 
 
 end
-
--- function ModelPed.setWeapon(ult_weapon)
---     print(ult_weapon)
---     local peds <const> = ped.get_all_peds()
---     for i = 1, #peds do
---         if not ped.is_ped_a_player(peds[i]) then
---             if HP_Entity.request_control(peds[i], 25) then
---                 weapon.give_delayed_weapon_to_ped(peds[i], ult_weapon, 10000, true)
---             end
---         end
---     end 
--- end
 
 
 function ModelPed.removeAllWeapon()
